86148ad97deec62b8f653b2db19db71bd73e77ec
linkeng <keng321>
2019-07-11 09:04:43 +0800

    11111111

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         int i = 4444444;
-        int i = 333333;
+        int ii = 333333;
         System.out.println("Hello World!");
     }
 }
