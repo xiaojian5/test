026f91b153fad448b5588b716a952c2dd2eb9184
xiaojian5 <42760010+xiaojian5@users.noreply.github.com>
2019-07-27 08:58:16 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         int i = 4444444;
-        int ii = 333333;
+        int ii = 332233;
+     int iii = 1332233;
         System.out.println("Hello World!");
     }
 }
